b674e38cea877866d65df7a73f226494da2f290e
Mariana Prazeres <mari_p@live.com.pt>
2018-10-29 16:17:08 -0400

    Now the minimum learning rate can be selected instead of being hard-coded

--- optim/adaptive.py
+++ optim/adaptive.py
@@ -9,31 +9,35 @@ class Adaptive(Optimizer):
         params (iterable): iterable of parameters to optimize or dicts defining
             parameter groups
         max_lr (float): maximum value the learning rate can take
+        min_lr (float): minimum value the learning rate can take
         fstar (float): a "reasonable" approximation of the true minimum value of 
                         the true loss. For example, values around the minimum 
                         value of the loss of a validation/test set after a good 
                         baseline run. 
         window (int): the number of gradient steps between each learning rate 
-                        update. For example, the equivalent to 20 epochs is a 
-                        good choice.
+                        update. For example, we have found 7500 steps is a 
+                        good choice, which is roughly 20 epochs on the CIFAR datasets.
 
     Example:
         >>> optimizer = Adaptive(model.parameters(), max_lr = 0.1, 
                             fstar = 0.200,
                             window = 20*grad_steps_in_epoch)
         >>> optimizer.zero_grad()
-        >>> loss_fn(model(data), target).backward()
+        >>> loss = loss_fn(model(data), target)
+        >>> runavg_loss = 0.9 *runavg_loss + 0.1*loss
+        >>> loss.backward()
         >>> optimizer.step(runavg_loss)
 
     .. note::
-        The optimizer requires an approximate value of the current training loss.
-        For smaller datasets this can be evaluated. If not, a running average of 
-        the current epochs batch losses might suffice. For test in cifar10 both 
-        options got good results, although the best fstar value are different.
+        The optimizer requires a value for the current true training loss (*not* the batch loss).
+        For smaller datasets this can be evaluated during test time after each epoch. 
+        If this is not possible then a running exponential average of 
+        the current epoch's batch losses might suffice. On CIFAR-10 both 
+        options give good results, although the best fstar values are different.
     """
 
-    def __init__(self, params, max_lr=required, fstar=0.0, 
-        window=10000):
+    def __init__(self, params, max_lr=required, min_lr = 0.00001, fstar=0.0, 
+        window=7500):
 
         if max_lr is not required and max_lr < 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
@@ -42,7 +46,7 @@ class Adaptive(Optimizer):
         if not isinstance(window,int):
             raise ValueError("Invalid window: {}".format(window))
         
-        defaults = dict(max_lr=max_lr, lr=max_lr, sumsq=0.0, 
+        defaults = dict(min_lr=min_lr, max_lr=max_lr, lr=max_lr, sumsq=0.0, 
             count=0, totalcount=0)
         
         self.fstar = fstar
@@ -71,7 +75,7 @@ class Adaptive(Optimizer):
             ## Update the learning rate according to the window value
             if group['totalcount'] % self.window == 0 and group['totalcount']>0:
                 dt = 2*(runavg_loss - self.fstar)/group['sumsq']
-                dt = max(min(dt, group['max_lr']), 0.00001) 
+                dt = max(min(dt, group['max_lr']), group['min_lr']) 
                 group['count'] = 0
                 group['sumsq'] = 0.0
                 print("\n[New learning rate]: %.2g\n"%dt)
